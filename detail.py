a116d2992ef4ab597be9fd9111a9f3016af5e985
hukish <hudsonhukish@gmail.com>
2020-04-19 21:45:09 +0300

    modified class detail

--- detail.py
+++ detail.py
@@ -10,3 +10,18 @@ import sys
 
 
 class Detail:
+
+        """
+    Class that generates new cases of details.
+    """
+
+    detail_list = []  # Empty detail list
+
+    def __init__(self, user_name, account_name,password,email):
+
+      # docstring removed for simplicity
+
+        self.user_name = user_name
+        self.account_name = account_name
+        self.account_password = password
+        self.email = email
