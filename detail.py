384b9e88cedce3c0e4c98376c3daed4fcda3bbb2
hukish <hudsonhukish@gmail.com>
2020-04-19 21:49:50 +0300

    add changes

--- detail.py
+++ detail.py
@@ -34,3 +34,17 @@ class Detail:
 
         Detail.detail_list.append(self)
    
+
+    def test_find_detail_by_password(self):
+        '''
+        test to check if we can find a detail by account password and display information
+        '''
+
+        self.new_detail.save_detail()
+        test_detail = Detail("Test", "user", "2222222222",
+                               "xyz@user.com")  # new detail
+        test_detail.save_detail()
+
+        found_detail = Detail.find_by_password("2222222222")
+
+        self.assertEqual(found_detail.email, test_detail.email)
